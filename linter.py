e3a855ccfb86efcdf1fe4f1859e4ec19adeef243
Daniel K <fredy.c@seznam.cz>
2016-03-09 15:19:52 +0100

    Use NodeLinter to support local stylelint
    Ref. https://github.com/FredyC/SublimeLinter-contrib-stylelint/commit/7bdaea8b316477e934018659641873a1080f83ca

--- linter.py
+++ linter.py
@@ -9,19 +9,32 @@
 #
 """This module exports the Stylelint plugin class."""
 
-import os
-
-from SublimeLinter.lint import Linter, util
+from SublimeLinter.lint import NodeLinter, util
 
 
-class Stylelint(Linter):
+class Stylelint(NodeLinter):
     """Provides an interface to stylelint."""
 
     syntax = ('css', 'css3', 'sass', 'scss', 'postcss')
-    cmd = ('node', os.path.dirname(os.path.realpath(__file__)) + '/stylelint_wrapper.js', '@')
+    executable = 'stylelint'
+    npm_name = 'stylelint'
     error_stream = util.STREAM_BOTH
     config_file = ('--config', '.stylelintrc', '~')
     tempfile_suffix = 'css'
     regex = (
         r'^\s*(?P<line>[0-9]+)\:(?P<col>[0-9]+)\s*(?:(?P<error>✖)|(?P<warning>⚠))\s*(?P<message>.+)'
     )
+
+    def cmd(self):
+        """Return a tuple with the command line to execute."""
+
+        path = self.executable_path
+
+        if not path:
+            result = self.context_sensitive_executable_path([self.executable])
+
+            if result[1]:
+                path = result[1]
+
+        command = [path]
+        return command
